b7778bc0550e3b56541123748008364f265a17e9
Bas Driessen <bas_driessen@hotmail.com>
2022-04-18 15:14:08 +0200

    move engine data project in correct folder on disk

---
+++ src/engine/Aurora.Engine.Data/Extensions/ElementModelExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using Aurora.Engine.Data.Models;
+
+namespace Aurora.Engine.Data.Extensions
+{
+    public static class ElementModelExtensions
+    {
+        /// <summary>
+        /// A shorthand to add a property to the list of properties of the element model.
+        /// </summary>
+        /// <param name="name">The name of the property.</param>
+        /// <param name="value">The value of the property.</param>
+        public static void AddProperty(this ElementModel model, string name, object value)
+        {
+            model.Properties.Add(name, value);
+        }
+    }
+}
