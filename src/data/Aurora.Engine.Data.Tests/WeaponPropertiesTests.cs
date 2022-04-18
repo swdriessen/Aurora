3a83172a7acbfedf5e35e53bf152c0d904cdbf20
Bas Driessen <bas_driessen@hotmail.com>
2022-04-18 14:39:01 +0200

    add weapon proficiency property

---
+++ src/data/Aurora.Engine.Data.Tests/WeaponPropertiesTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Aurora.Engine.Data.Models;
+using Aurora.Engine.Data.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aurora.Engine.Data.Tests
+{
+    [TestClass]
+    public class WeaponPropertiesTests
+    {
+        private readonly ElementPropertiesModel elementProperties = new();
+
+        [TestMethod]
+        public void WeaponProperties_ShouldHaveNoProficiency_WhenPropertyIsOmitted()
+        {
+            // arrange
+            // act
+            var properties = new WeaponProperties(elementProperties);
+
+            // assert
+            Assert.AreEqual(string.Empty, properties.Proficiency);
+        }
+
+        [TestMethod]
+        public void WeaponProperties_ShouldReturnProficiency_WhenPropertyIsSet()
+        {
+            // arrange
+            var proficiency = "ID_PROFICIENCY_LONGSWORD";
+            elementProperties.Set(ElementStrings.Properties.WeaponProficiency, proficiency);
+
+            // act
+            var properties = new WeaponProperties(elementProperties);
+
+            // assert
+            Assert.AreEqual(proficiency, properties.Proficiency);
+        }
+
+        [TestMethod]
+        public void WeaponProperties_ShouldReturnProficiencies_WhenPropertyIsSetMultipleTimes()
+        {
+            // arrange
+            var proficiency1 = "ID_PROFICIENCY_LONGSWORD";
+            var proficiency2 = "ID_PROFICIENCY_CUSTOM_LONGSWORD";
+            elementProperties.Set(ElementStrings.Properties.WeaponProficiency, $"{proficiency1};{proficiency2}");
+
+            // act
+            var properties = new WeaponProperties(elementProperties);
+
+            // assert
+            var proficiencies = properties.GetProficiencies();
+            Assert.IsTrue(proficiencies.Contains(proficiency1));
+            Assert.IsTrue(proficiencies.Contains(proficiency2));
+        }
+    }
+}
---
+++ src/data/Aurora.Engine.Data/WeaponProperties.cs
@@ -0,0 +1,25 @@
+﻿using Aurora.Engine.Data.Models;
+using Aurora.Engine.Data.Strings;
+
+namespace Aurora.Engine.Data
+{
+    public class WeaponProperties
+    {
+        private readonly ElementPropertiesModel properties;
+
+        public WeaponProperties(ElementPropertiesModel properties)
+        {
+            this.properties = properties;
+        }
+
+        public string Proficiency
+        {
+            get { return properties.Get(ElementStrings.Properties.WeaponProficiency, string.Empty); }
+        }
+
+        public IEnumerable<string> GetProficiencies()
+        {
+            return Proficiency.Split(ElementStrings.Properties.PropertiesSeparator);
+        }
+    }
+}
