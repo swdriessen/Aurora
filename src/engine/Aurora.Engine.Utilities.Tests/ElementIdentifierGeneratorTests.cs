c748489e9d9b8f36606e99b85f06e69a6129387c
Bas Driessen <bas_driessen@hotmail.com>
2022-04-24 15:12:09 +0200

    create a generator for identifiers

---
+++ src/engine/Aurora.Engine.Utilities.Tests/ElementIdentifierGeneratorTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aurora.Engine.Utilities.Tests
+{
+    [TestClass]
+    public class ElementIdentifierGeneratorTests
+    {
+        private readonly ElementIdentifierGenerator generator;
+        private readonly string source = "Player's Handbook";
+        private readonly string author = "Wizards of the Coast";
+
+        public ElementIdentifierGeneratorTests()
+        {
+            generator = new ElementIdentifierGenerator(options =>
+            {
+                options.IncludeSourceAbbreviation = true;
+                options.IncludeAuthorAbbreviation = true;
+            });
+        }
+
+        [TestMethod]
+        public void Generator_ShouldGenerateCompleteIdentifier_WhenAllParametersAreProvided()
+        {
+            // arrange
+            var type = "Class Feature";
+            var name = "Rage";
+            var parent = "Barbarian";
+
+            // act
+            var identifier = generator.GenerateIdentifier(source, author, type, name, parent);
+
+            // assert
+            Assert.AreEqual("ID_PHB_WOTC_CLASS_FEATURE_BARBARIAN_RAGE", identifier);
+        }
+
+        [TestMethod]
+        public void Generator_ShouldIgnoreParent_WhenItIsEmptyOrNull()
+        {
+            // arrange
+            var type = "Class Feature";
+            var name = "Rage";
+            var parent = string.Empty;
+
+            // act
+            var identifier = generator.GenerateIdentifier(source, author, type, name, parent);
+
+            // assert
+            Assert.AreEqual("ID_PHB_WOTC_CLASS_FEATURE_RAGE", identifier);
+        }
+
+        [TestMethod]
+        public void Generator_ShouldThrowException_WhenNameIsOmitted()
+        {
+            // arrange
+            // act
+            var type = "Class Feature";
+            var name = string.Empty;
+            var parent = "Barbarian";
+
+            // assert
+            Assert.ThrowsException<ArgumentException>(() => { generator.GenerateIdentifier(source, author, type, name, parent); }, "Expected an exception as the name is required.");
+        }
+
+        [TestMethod]
+        public void Generator_ShouldThrowException_WhenTypeIsOmitted()
+        {
+            // arrange
+            // act
+            var type = string.Empty;
+            var name = "Rage";
+            var parent = "Barbarian";
+
+            // assert
+            Assert.ThrowsException<ArgumentException>(() => { generator.GenerateIdentifier(source, author, type, name, parent); }, "Expected an exception as the type is required.");
+        }
+
+        [TestMethod]
+        public void Generator_ShouldGenerateCompleteIdentifier_WhenParametersWithSpecialCharactersAreProvided()
+        {
+            // arrange
+            var source = "Baldur's Gate: Descent into Avernus";
+            var author = "Asmodeus, Lord of Nessus";
+            var type = "Class Feature";
+            var name = "Barbarian's Rage";
+            var parent = "Abyssal Barbarian";
+
+            // act
+            var identifier = generator.GenerateIdentifier(source, author, type, name, parent);
+
+            // assert
+            Assert.AreEqual("ID_BGDIA_ALON_CLASS_FEATURE_ABYSSAL_BARBARIAN_BARBARIANS_RAGE", identifier);
+        }
+    }
+}
---
+++ src/engine/Aurora.Engine.Utilities/ElementIdentifierGenerator.cs
@@ -0,0 +1,92 @@
+﻿namespace Aurora.Engine.Utilities
+{
+    public class ElementIdentifierGenerator
+    {
+        private readonly AbbreviationGenerator abbreviationGenerator;
+        private readonly GenerationOptions options = new();
+
+        public ElementIdentifierGenerator(Action<GenerationOptions>? configure = null)
+        {
+            configure?.Invoke(options);
+
+            abbreviationGenerator = new AbbreviationGenerator(options =>
+            {
+                options.Exceptions.Add("Internal");
+                options.Presets.Add("Player's Handbook", "PHB");
+            });
+        }
+
+        /// <summary>
+        /// Generates an identifier for an element. Include a source and author to allow for a more unique identifier.
+        /// </summary>
+        /// <param name="source">The full name of the source.</param>
+        /// <param name="author">The full name of the author.</param>
+        /// <param name="type">The type of the element. This cannot be empty.</param>
+        /// <param name="name">The name of the element. This cannot be empty.</param>
+        /// <param name="parentName">The optional name of the parent element.</param>
+        /// <returns>The generated identifier.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public string GenerateIdentifier(string source, string author, string type, string name, string? parentName = null)
+        {
+            if (string.IsNullOrWhiteSpace(type))
+            {
+                throw new ArgumentException($"'{nameof(type)}' cannot be null or whitespace.", nameof(type));
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException($"'{nameof(name)}' cannot be null or whitespace.", nameof(name));
+            }
+
+            var parts = new List<string>
+            {
+                options.IdentifierPrefix.Trim()
+            };
+
+            if (options.IncludeSourceAbbreviation)
+            {
+                parts.Add(abbreviationGenerator.Generate(source));
+            }
+
+            if (options.IncludeAuthorAbbreviation)
+            {
+                parts.Add(abbreviationGenerator.Generate(author));
+            }
+
+            parts.Add(Sanatize(type));
+
+            if (!string.IsNullOrEmpty(parentName))
+            {
+                parts.Add(Sanatize(parentName));
+            }
+
+            parts.Add(Sanatize(name));
+
+            return string.Join("_", parts).ToUpperInvariant();
+        }
+
+        private static string Sanatize(string input)
+        {
+            return input.Sanitize().Replace(" ", "_");
+        }
+
+        public class GenerationOptions
+        {
+            /// <summary>
+            /// Gets or sets the prefix for the identifier to be generated. (Default is 'ID')
+            /// </summary>
+            public string IdentifierPrefix { get; set; } = "ID";
+
+            /// <summary>
+            /// Gets or sets a value whether or not to include the source abbreviation in the generated identifier. (Default is true)
+            /// </summary>
+            public bool IncludeSourceAbbreviation { get; set; } = true;
+
+            /// <summary>
+            /// Gets or sets a value whether or not to include the author abbreviation in the generated identifier. (Default is true)
+            /// </summary>
+            public bool IncludeAuthorAbbreviation { get; set; } = true;
+        }
+    }
+
+}
