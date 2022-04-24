ec0b296c90d41d718f0dbaded5882f33f1c6689e
Bas Driessen <bas_driessen@hotmail.com>
2022-04-24 14:27:47 +0200

    add extension method to verify a string is considered to be an element id

---
+++ src/engine/Aurora.Engine.Utilities.Tests/StringExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aurora.Engine.Utilities.Tests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void IsElementIdentifier_ShouldReturnTrue_WhenElementIdEvaluated()
+        {
+            // arrange
+            var input = "ID_INTERNAL_IDENTIFIER";
+
+            // act
+            var output = input.IsElementIdentifier();
+
+            // assert
+            Assert.IsTrue(output);
+        }
+    }
+}
--- src/engine/Aurora.Engine.Utilities/StringExtensions.cs
+++ src/engine/Aurora.Engine.Utilities/StringExtensions.cs
@@ -18,5 +18,15 @@ namespace Aurora.Engine.Utilities
 
             return input.Trim();
         }
+
+        /// <summary>
+        /// Gets a value whether the input string is an element identifier, indicated by starting with 'ID_'.
+        /// </summary>
+        /// <param name="input">The input string to be evaluated.</param>
+        /// <returns>True when the string is considered to be an element id.</returns>
+        public static bool IsElementIdentifier(this string input)
+        {
+            return input.StartsWith("ID_");
+        }
     }
 }
