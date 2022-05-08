d50989b885a8204abad3362bcc9388f2473a6d9b
Bas Driessen <bas_driessen@hotmail.com>
2022-05-08 20:17:06 +0200

    include more element types

--- src/engine/Aurora.Engine.Data/Strings/ElementTypeConstants.cs
+++ src/engine/Aurora.Engine.Data/Strings/ElementTypeConstants.cs
@@ -2,22 +2,85 @@
 {
     public class ElementTypeConstants
     {
+        // (reserved) internal application types
         public const string Internal = "Internal";
+        public const string Option = "Option";
+        public const string Source = "Source";
+        public const string Information = "Information";
+        public const string Rule = "Rule";
+        public const string Assignment = "Assignment";
+        public const string Grants = "Grants";
+        public const string Support = "Support";
+        public const string Ignore = "Ignore";
+        public const string EquipmentCategory = "Equipment Category";
 
+        // system types
+        public const string Ability = "Ability";
+        public const string SavingThrow = "Saving Throw";
+        public const string Skill = "Skill";
+        public const string DamageType = "Damage Type";
+        public const string MagicSchool = "Magic School";
+        public const string GenericFeature = "Feature";
+        public const string GenericTrait = "Trait";
+        public const string AbilityScoreIncrease = "Ability Score Increase";
+        public const string Proficiency = "Proficiency";
+        public const string Condition = "Condition";
+        public const string Alignment = "Alignment";
+        public const string Level = "Level";
+        public const string Size = "Size";
+        public const string Vision = "Vision";
+
+        // race
+        public const string Race = "Race";
+        public const string RaceTrait = "Race Trait";
+        public const string RaceVariant = "Race Variant";
+        public const string SubRace = "Sub Race";
+
+        // class
+        public const string Class = "Class";
+        public const string ClassFeature = "Class Feature";
+        public const string Archetype = "Archetype";
+        public const string ArchetypeFeature = "Archetype Feature";
+
+        // background
+        public const string Background = "Background";
+        public const string BackgroundFeature = "Background Feature";
+        public const string BackgroundCharacteristics = "Background Characteristics";
+        public const string BackgroundVariant = "Background Variant";
+
+        // feat
+        public const string Feat = "Feat";
+        public const string FeatFeature = "Feat Feature";
+
+        // language
+        public const string Language = "Language";
+        public const string LanguageFeature = "Language Feature";
+
+        // spells
+        public const string Spell = "Spell";
+
+        // equipment types
         public const string Item = "Item";
         public const string MagicItem = "Magic Item";
         public const string Armor = "Armor";
         public const string Weapon = "Weapon";
 
-
-        public const string Ability = "Ability";
-        public const string SavingThrow = "Saving Throw";
-        public const string Skill = "Skill";
-
         public const string WeaponGroup = "Weapon Group";
         public const string WeaponProperty = "Weapon Property";
+        public const string WeaponCategory = "Weapon Category";
+        public const string ArmorGroup = "Armor Group";
+        public const string ArmorProperty = "Armor Property";
 
-        public const string DamageType = "Damage Type";
+        // companion
+        public const string Companion = "Companion";
+        public const string CompanionAction = "Companion Action";
+        public const string CompanionReaction = "Companion Reaction";
+        public const string CompanionTrait = "Companion Trait";
 
+        // deity
+        public const string Deity = "Deity";
+
+        // dragonmark
+        public const string Dragonmark = "Dragonmark";
     }
 }
