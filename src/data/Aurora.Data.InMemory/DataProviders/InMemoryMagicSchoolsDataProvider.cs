52632c7ea2c044df1bfac44e3d5cdd1db1392dc8
Bas Driessen <bas_driessen@hotmail.com>
2022-05-14 20:47:08 +0200

    add magic schools to test data

---
+++ src/data/Aurora.Data.InMemory/DataProviders/InMemoryMagicSchoolsDataProvider.cs
@@ -0,0 +1,40 @@
+﻿using Aurora.Engine.Data.Interfaces;
+using Aurora.Engine.Data.Models;
+
+namespace Aurora.Data.InMemory.DataProviders
+{
+    internal class InMemoryMagicSchoolsDataProvider : IDataProvider
+    {
+        public List<ElementModel> GetElements()
+        {
+            return new List<ElementModel>(CreateMagicSchools());
+        }
+
+        public List<ElementModel> GetElements(string type)
+        {
+            return GetElements().Where(x => x.Type.Equals(type)).ToList();
+        }
+
+        private static List<ElementModel> CreateMagicSchools()
+        {
+            var elements = new List<ElementModel>
+            {
+                new ElementModel() { Name = "Abjuration" },
+                new ElementModel() { Name = "Conjuration" },
+                new ElementModel() { Name = "Divination" },
+                new ElementModel() { Name = "Enchantment" },
+                new ElementModel() { Name = "Evocation" },
+                new ElementModel() { Name = "Illusion" },
+                new ElementModel() { Name = "Necromancy" },
+                new ElementModel() { Name = "Transmutation" },
+            };
+
+            elements.ForEach(e =>
+            {
+                e.Type = "Magic School";
+            });
+
+            return elements;
+        }
+    }
+}
--- src/data/Aurora.Data.InMemory/DataProviders/InMemorySpellsDataProvider.cs
+++ src/data/Aurora.Data.InMemory/DataProviders/InMemorySpellsDataProvider.cs
@@ -18,46 +18,44 @@ namespace Aurora.Data.InMemory.DataProviders
 
         private static List<ElementModel> CreateSpells()
         {
-            var elements = new List<ElementModel>();
-
-            var spells = new List<ElementModel>
+            var elements = new List<ElementModel>
             {
+                new ElementModel()
+                {
+                    Name = "Eldritch Blast",
+                    Properties = new()
+                    {
+                        { ElementConstants.SpellProperties.MagicSchool, "Evocation" },
+                        { ElementConstants.SpellProperties.Level, 0 },
+                        { ElementConstants.SpellProperties.CastingTime, "1 action" },
+                        { ElementConstants.SpellProperties.Range, "120 feet" },
+                        { ElementConstants.SpellProperties.SomaticComponent, true },
+                        { ElementConstants.SpellProperties.VerbalComponent, true },
+                        { ElementConstants.SpellProperties.Duration, "Instantaneous" },
+                    }
+                },
                 new ElementModel()
                 {
                     Name = "Fireball",
                     Properties = new()
                     {
+                        { ElementConstants.SpellProperties.MagicSchool, "Evocation" },
+                        { ElementConstants.SpellProperties.Level, 3 },
                         { ElementConstants.SpellProperties.CastingTime, "1 action" },
-                        { ElementConstants.SpellProperties.Range, "120 feet" },
-                        { ElementConstants.SpellProperties.Duration, "Instantaneous" },
+                        { ElementConstants.SpellProperties.Range, "150 feet" },
                         { ElementConstants.SpellProperties.SomaticComponent, true },
                         { ElementConstants.SpellProperties.VerbalComponent, true },
                         { ElementConstants.SpellProperties.MaterialComponent, true },
-                        { ElementConstants.SpellProperties.MaterialComponentDescription, "a brimstone" },
-                    }
-                },
-                new ElementModel()
-                {
-                    Name = "Iceball",
-                    Properties = new()
-                    {
-                        { ElementConstants.SpellProperties.CastingTime, "1 action" },
-                        { ElementConstants.SpellProperties.Range, "120 feet" },
+                        { ElementConstants.SpellProperties.MaterialComponentDescription, "a tiny ball of bat guano and sulfur" },
                         { ElementConstants.SpellProperties.Duration, "Instantaneous" },
-                        { ElementConstants.SpellProperties.SomaticComponent, true },
-                        { ElementConstants.SpellProperties.VerbalComponent, true },
-                        { ElementConstants.SpellProperties.MaterialComponent, true },
-                        { ElementConstants.SpellProperties.MaterialComponentDescription, "a snowflake" },
                     }
                 }
             };
 
-            spells.ForEach(e =>
+            elements.ForEach(e =>
             {
                 e.Type = "Spell";
             });
-
-            elements.AddRange(spells);
 
             return elements;
         }
