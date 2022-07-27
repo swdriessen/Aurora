26d3ee7245c097cc5869cc35e4d94e35e944397e
Bas Driessen <bas_driessen@hotmail.com>
2022-07-28 00:52:20 +0200

    add description component

--- src/core/Aurora.Core/Components/ComponentExtensions.cs
+++ src/core/Aurora.Core/Components/ComponentExtensions.cs
@@ -1,29 +1,61 @@
 ﻿using Aurora.Core.Abstractions;
 using Aurora.Core.Components.Extractable;
 using Aurora.Core.Components.Item;
+using Aurora.Engine.Components.Description;
 
-namespace Aurora.Core.Components;
+namespace Aurora.Engine.Components;
 
 public static class ComponentExtensions
 {
+    /// <summary>
+    /// Adds the <see cref="DescriptionComponent"/> to the list of components of the element.
+    /// </summary>
+    /// <param name="element">The element to add the component to.</param>
+    /// <returns>The element.</returns>
+    public static IElement AddDescriptionComponent(this IElement element)
+    {
+        element.Components.AddComponent(new DescriptionComponent());
+        return element;
+    }
+
+    /// <summary>
+    /// Adds the <see cref="ItemComponent"/> to the list of components of the element.
+    /// </summary>
+    /// <param name="element">The element to add the component to.</param>
+    /// <returns>The element.</returns>
     public static IElement AddItemComponent(this IElement element)
     {
         element.Components.AddComponent(new ItemComponent());
         return element;
     }
 
+    /// <summary>
+    /// Adds the <see cref="WeightComponent"/> to the list of components of the element.
+    /// </summary>
+    /// <param name="element">The element to add the component to.</param>
+    /// <returns>The element.</returns>
     public static IElement AddWeightComponent(this IElement element)
     {
         element.Components.AddComponent(new WeightComponent());
         return element;
     }
 
+    /// <summary>
+    /// Adds the <see cref="CostComponent"/> to the list of components of the element.
+    /// </summary>
+    /// <param name="element">The element to add the component to.</param>
+    /// <returns>The element.</returns>
     public static IElement AddCostComponent(this IElement element)
     {
         element.Components.AddComponent(new CostComponent());
         return element;
     }
 
+    /// <summary>
+    /// Adds the <see cref="ExtractableComponent"/> to the list of components of the element.
+    /// </summary>
+    /// <param name="element">The element to add the component to.</param>
+    /// <returns>The element.</returns>
     public static IElement AddExtractableComponent(this IElement element)
     {
         element.Components.AddComponent(new ExtractableComponent());
---
+++ src/core/Aurora.Core/Components/Description/DescriptionComponent.cs
@@ -0,0 +1,11 @@
+﻿using Aurora.Core;
+
+namespace Aurora.Engine.Components.Description;
+
+public class DescriptionComponent : IElementComponent
+{
+    /// <summary>
+    /// Gets or sets the description of the element.
+    /// </summary>
+    public string Content { get; set; } = string.Empty;
+}
