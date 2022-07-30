c09c2ac95ddb527a3325ffc9909d28966adb4bf2
Bas Driessen <bas_driessen@hotmail.com>
2022-07-30 18:12:49 +0200

    add basic rules (include, selection, statistic)

---
+++ src/core/Aurora.Core/Rules/ElementRuleBase.cs
@@ -0,0 +1,12 @@
+﻿using Aurora.Engine.Abstractions;
+using Aurora.Engine.Elements;
+
+namespace Aurora.Engine.Rules;
+
+public abstract class ElementRuleBase : IElementRule
+{
+    /// <summary>
+    /// Gets a collection of properties associated with this rule.
+    /// </summary>
+    public Properties Properties { get; } = new();
+}
---
+++ src/core/Aurora.Core/Rules/SelectionRule.cs
@@ -0,0 +1,17 @@
+﻿namespace Aurora.Engine.Rules;
+
+/// <summary>
+/// Initializes a new instance of the <see cref="SelectionRule"/> class.
+/// </summary>
+public class SelectionRule : ElementRuleBase
+{
+    public SelectionRule(string elementType)
+    {
+        ElementType = elementType;
+    }
+
+    /// <summary>
+    /// Gets or sets the element type for the selection rule.
+    /// </summary>
+    public string ElementType { get; set; }
+}
