7fc8febb39619311ef7b979138fc6b67ceb22060
Bas Driessen <bas_driessen@hotmail.com>
2022-08-02 14:35:28 +0200

    add a predicate based interface for the element provider

---
+++ src/engine/Aurora.Engine.Elements.Abstractions/IElementDataProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace Aurora.Engine.Elements.Abstractions;
+
+public interface IElementDataProvider
+{
+    /// <summary>
+    /// Gets an element that matches the predicate.
+    /// </summary>
+    /// <param name="predicate">The predicate to match your element.</param>
+    /// <returns>The element if found, otherwise returns null.</returns>
+    IElement? GetElement(Func<IElement, bool> predicate);
+
+    /// <summary>
+    /// Gets a collection of elements that matches the predicate.
+    /// </summary>
+    /// <param name="predicate">The predicate to match your elements.</param>
+    /// <returns>The list of elements that match your predicate, otherwise returns an empty collection.</returns>
+    IEnumerable<IElement> GetElements(Func<IElement, bool> predicate);
+}
