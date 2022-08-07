fb6f4e438a0614d2e96617e29ed0e362b5f154dd
Bas Driessen <bas_driessen@hotmail.com>
2022-08-07 23:33:01 +0200

    start adding some entry points to start some basic testing

---
+++ src/engine/Aurora.Engine/Aurora.Engine/IApplicationHost.cs
@@ -0,0 +1,11 @@
+﻿namespace Aurora.Engine;
+
+public interface IApplicationHost
+{
+    // generic host for the application (use a generic host)
+    // default configuration and services
+
+    // usage:
+    // created at startup
+    // resolve an IEngineHost for a requested system
+}
---
+++ src/engine/Aurora.Engine/Aurora.Engine/IEngineHost.cs
@@ -0,0 +1,16 @@
+﻿namespace Aurora.Engine;
+
+public interface IEngineHost
+{
+    // single host for the character generation (created with builder similar to a generic host)
+
+    // application hooks to interact with the application host (e.g. error handling, context switching)
+    // application configuration and services where applicable
+    // engine and system configuration
+    // engine and system services
+    // character configuration
+    // source restriction information
+
+    // usage:
+    // configure for a specific system (e.g. DND5E), start, stop
+}
