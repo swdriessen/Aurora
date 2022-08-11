5d6c4adf9eada90d53031f57e743ed592194c4dc
Bas Driessen <bas_driessen@hotmail.com>
2022-08-11 23:38:46 +0200

    add ConfigureComponent in ElementExtension

---
+++ src/engine/Aurora.Engine.Elements.Abstractions/ElementExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Aurora.Engine.Elements.Abstractions;
+
+public static class ElementExtensions
+{
+    /// <summary>
+    /// Configure an element component from the components collection. When the component already exists it configures the existing component, otherwise it will add a new component and configure that.
+    /// </summary>
+    public static IElement ConfigureComponent<T>(this IElement element, Action<T> component) where T : class, IElementComponent, new()
+    {
+        if (element.Components.TryGetComponent<T>(out var existingComponent))
+        {
+            component(existingComponent);
+        }
+        else
+        {
+            var instance = new T();
+            component(instance);
+            element.Components.AddComponent(instance);
+        }
+
+        return element;
+    }
+}
