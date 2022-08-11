af29ab6fd6ae6db109faf1095f567ac391e8a6e3
Bas Driessen <bas_driessen@hotmail.com>
2022-08-11 22:21:46 +0200

    add ElementAggregate for registration

---
+++ src/engine/Aurora.Engine.Tests/ElementAggregationTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Aurora.Engine.Elements;
+using Aurora.Engine.Generation;
+
+namespace Aurora.Engine.Tests;
+
+[TestClass]
+public class ElementAggregationTest
+{
+    [TestMethod]
+    public void ElementAggregate_ShouldInitializeWithElementReference()
+    {
+        // arrange
+        var element = new Element()
+        {
+            Identifier = "ID_A",
+            Name = "Element A",
+            ElementType = "Type"
+        };
+
+        // act
+        var aggregate = ElementAggregate.ForElement(element);
+
+        // assert
+        Assert.AreSame(element, aggregate.Element);
+    }
+
+    [TestMethod]
+    public void ElementAggregate_ShouldInitializeWithUniqueIdentifier()
+    {
+        // arrange
+        var element = new Element()
+        {
+            Identifier = "ID_A",
+            Name = "Element A",
+            ElementType = "Type"
+        };
+
+        // act
+        var aggregate = ElementAggregate.ForElement(element);
+        Debug.WriteLine(aggregate);
+
+        // assert
+        Assert.IsNotNull(aggregate.Element);
+        Assert.IsFalse(string.IsNullOrWhiteSpace(aggregate.Identifier));
+        Assert.AreNotEqual("00000000-0000-0000-0000-000000000000", aggregate.Identifier, "Expected the guid to be initialized with a unique id.");
+    }
+}
---
+++ src/engine/Aurora.Engine/Generation/ElementAggregate.cs
@@ -0,0 +1,40 @@
+﻿using Aurora.Engine.Elements.Abstractions;
+
+namespace Aurora.Engine.Generation;
+
+/// <summary>
+/// The aggregated element represents an element that has been assigned to a character. It holds additional information surrouding the assignment of the element as well as it's current state.
+/// </summary>
+public class ElementAggregate
+{
+    public ElementAggregate(IElement element)
+    {
+        Element = element;
+        Identifier = Guid.NewGuid().ToString();
+    }
+
+    /// <summary>
+    /// Gets the unique identifier for this aggregate.
+    /// </summary>
+    public string Identifier { get; }
+
+    /// <summary>
+    /// Gets the element associated with this aggregate.
+    /// </summary>
+    public IElement Element { get; }
+
+    /// <summary>
+    /// Create an <see cref="ElementAggregate"/> for the specified <see cref="IElement"/>.
+    /// </summary>
+    /// <param name="element">The element for which to create the aggregate.</param>
+    /// <returns>The new element aggregate.</returns>
+    public static ElementAggregate ForElement(IElement element)
+    {
+        return new ElementAggregate(element);
+    }
+
+    public override string ToString()
+    {
+        return $"[{Identifier}] {Element.Name}";
+    }
+}
