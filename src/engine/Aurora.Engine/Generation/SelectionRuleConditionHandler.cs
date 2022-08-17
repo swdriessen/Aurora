61795e5c95ded8440dec87716d0f37eb3b998eb3
Bas Driessen <bas_driessen@hotmail.com>
2022-08-18 00:10:37 +0200

    add the SelectionRuleConditionHandler implementation

---
+++ src/engine/Aurora.Engine/Generation/SelectionRuleConditionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Aurora.Engine.Elements.Abstractions;
+using Aurora.Engine.Elements.Rules;
+using Microsoft.Extensions.Logging;
+
+namespace Aurora.Engine.Generation;
+
+/// <summary>
+/// Initializes a new instance of the <see cref="SelectionRuleConditionHandler"/> class.
+/// </summary>
+public sealed class SelectionRuleConditionHandler : IRuleConditionHandler<SelectionRule>
+{
+    private readonly ILogger<SelectionRuleConditionHandler> logger;
+
+    public SelectionRuleConditionHandler(ILogger<SelectionRuleConditionHandler> logger)
+    {
+        this.logger = logger;
+    }
+
+    public bool EvaluateCondition(SelectionRule rule)
+    {
+        // TODO: evaluate all conditions of the rule (e.g. level requirement)
+        logger.LogWarning("evaluation for selection rules not implemented, always evaluates to true");
+        return true;
+    }
+}
