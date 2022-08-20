8ca83a97d3b11e83118e4c8a0ccff5cf6eeac308
Bas Driessen <bas_driessen@hotmail.com>
2022-08-20 23:38:02 +0200

    add tests for processing level element and existing rule

--- src/engine/Aurora.Engine.Tests/ProgressionManagerTest.cs
+++ src/engine/Aurora.Engine.Tests/ProgressionManagerTest.cs
@@ -1,5 +1,6 @@
 ﻿using Aurora.Engine.Elements;
 using Aurora.Engine.Elements.Abstractions;
+using Aurora.Engine.Elements.Components.Level;
 using Aurora.Engine.Elements.Components.Rules;
 using Aurora.Engine.Elements.Rules;
 using Aurora.Engine.Generation;
@@ -27,16 +28,37 @@ public class ProgressionManagerTest
     private readonly SelectionRule rule1 = new("Language");
     private readonly SelectionRule rule2 = new("Feat");
 
+    private bool rule1HandlerCreated = false;
+    private bool rule2HandlerCreated = false;
+
     [TestInitialize]
     public void Setup()
     {
         selectionHandlerMock.SetupGet(x => x.UniqueIdentifier).Returns(Guid.NewGuid());
 
-        selectionHandlerManagerMock.Setup(x => x.Create(It.IsAny<SelectionRule>()))
-            .Returns<SelectionRule>((rule) => { return new List<IElementSelectionHandler> { selectionHandlerMock.Object }; });
-
         selectionHandlerManagerMock.Setup(x => x.HandlerExistsForSelectionRule(It.IsAny<Guid>()))
-            .Returns<Guid>((rule) => { return false; });
+            .Returns<Guid>((id) =>
+            {
+                if (id == rule1.UniqueIdentifier)
+                {
+                    return rule1HandlerCreated;
+                }
+
+                if (id == rule2.UniqueIdentifier)
+                {
+                    return rule2HandlerCreated;
+                }
+
+                return false;
+            });
+
+        selectionHandlerManagerMock.Setup(x => x.Create(It.IsAny<SelectionRule>()))
+            .Returns<SelectionRule>((rule) => { return new List<IElementSelectionHandler> { selectionHandlerMock.Object }; })
+            .Callback<SelectionRule>(rule =>
+            {
+                rule1HandlerCreated = rule.UniqueIdentifier == rule1.UniqueIdentifier;
+                rule2HandlerCreated = rule.UniqueIdentifier == rule2.UniqueIdentifier;
+            });
 
         engine = EngineHostBuilder.CreateDefaultBuilder()
             //.ConfigureScenarioDefaults()
@@ -67,7 +89,7 @@ public class ProgressionManagerTest
     [TestCleanup]
     public void Teardown()
     {
-        //selectionHandlerManagerMock.VerifyNoOtherCalls();
+        selectionHandlerManagerMock.VerifyNoOtherCalls();
 
         engine.Dispose();
     }
@@ -108,6 +130,56 @@ public class ProgressionManagerTest
         selectionHandlerManagerMock.Verify(x => x.Create(rule2), Times.Exactly(1), "Expected the progress manager to process the selection rule 2.");
         selectionHandlerManagerMock.Verify(x => x.Create(It.IsAny<SelectionRule>()), Times.Exactly(2), "Expected the progress manager to process both selection rules.");
         selectionHandlerMock.Verify(x => x.Initialize(), Times.Exactly(2), "Expected the created selection handlers both to be initialized.");
+    }
+
+    [TestMethod]
+    public void ProgressionManager_ShouldNotCreateAnotherHandler_WhenProcessingTheSameSelectionRule()
+    {
+        // arrange
+        manager = engine.Services.GetRequiredService<IProgressionManager>();
+        var aggregate1 = ElementAggregate.ForElement(element);
+        var aggregate2 = ElementAggregate.ForElement(element);
+
+        // act
+        manager.Process(aggregate1);
+        manager.Process(aggregate2);
+
+        // assert
+        selectionHandlerManagerMock.Verify(x => x.HandlerExistsForSelectionRule(It.IsAny<Guid>()), Times.Exactly(2), "Expected the to check if the rule exists before creating it.");
+        selectionHandlerManagerMock.Verify(x => x.Create(It.IsAny<SelectionRule>()), Times.Once, "Expected the progress manager to process the selection rule only once.");
+    }
+
+    [TestMethod]
+    public void ProgressionManager_ShouldHaveZeroProgressionValue_WhenInitialized()
+    {
+        // arrange + act
+        var expectedProgressionValue = 0;
+        manager = engine.Services.GetRequiredService<IProgressionManager>();
+
+        // assert
+        Assert.AreEqual(expectedProgressionValue, manager.CurrentProgressionValue);
+    }
+
+    [TestMethod]
+    public void ProgressionManager_ShouldHaveIncreasedProgressionValue_WhenLevelElementProcessed()
+    {
+        // arrange
+        var expectedProgressionValue = 1;
+        manager = engine.Services.GetRequiredService<IProgressionManager>();
+
+        var level = Element.Compose(element =>
+        {
+            element.ElementType = "Level";
+            element.ConfigureComponent<LevelComponent>(component => { component.Level = 1; });
+        });
+
+        var aggregate = ElementAggregate.ForElement(level);
+
+        // act
+        manager.Process(aggregate);
+
+        // assert
+        Assert.AreEqual(expectedProgressionValue, manager.CurrentProgressionValue);
     }
 }
 
